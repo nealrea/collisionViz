e715940215f79dc380197b1a0fd1dffab57b0c2a
Neal Rea <nealrea@Neal-Reas-MacBook-Pro.local>
2017-04-26 02:04:26 -0400

    added functions for bike, pedestrian, motorist totals

--- app/compute.py
+++ app/compute.py
@@ -12,3 +12,16 @@ def borough_totals():
 
 def zip_totals():
     return collisions['ZIP CODE'].value_counts().to_dict()
+
+def bike_totals(collRate):
+	df = collisions[collisions['Collision Rate'] == collRate]
+	return {'Injuries': df['NUMBER OF CYCLIST INJURED'].sum(axis=0), 'Deaths': df['NUMBER OF CYCLIST KILLED'].sum(axis=0)}
+
+def ped_totals(collRate)
+	df = collisions[collisions['Collision Rate'] == collRate]
+	return {'Injuries': df['NUMBER OF PEDESTRIANS INJURED'].sum(axis=0), 'Deaths': df['NUMBER OF PEDESTRIANS KILLED'].sum(axis=0)}
+
+def motorist_totals(collRate)
+	df = collisions[collisions['Collision Rate'] == collRate]
+	return {'Injuries': df['NUMBER OF MOTORIST INJURED'].sum(axis=0), 'Deaths': df['NUMBER OF MOTORIST KILLED'].sum(axis=0)}
+
