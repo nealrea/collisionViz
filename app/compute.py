02430a81ce09e1f087601b6cd40fead6b0d6e366
John Spencer <johnspencer15@gmail.com>
2017-04-26 01:15:52 -0400

    add totals for boroughs and zips

--- app/compute.py
+++ app/compute.py
@@ -1,11 +1,14 @@
 import pandas as pd
 
 collisions = pd.read_csv('collisions.csv')
-boroughs = ['BRONX', 'BROOKLYN', 'QUEENS', 'MANHATTAN', 'STATEN ISLAND']
+boroughs = ['Bronx', 'Brooklyn', 'Queens', 'Manhattan', 'Staten Island']
 
 def borough_totals():
     borough_total = {}
     for borough in boroughs:
-        borough_total[borough] = len(collisions[collisions['BOROUGH'] == borough])
+        borough_total[borough] = len(collisions[collisions['BOROUGH'] == borough.upper()])
 
     return borough_total
+
+def zip_totals():
+    return collisions['ZIP CODE'].value_counts().to_dict()
