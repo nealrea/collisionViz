7697decbcdf8fbbf9b08a4a02ef1f3241580230e
John Spencer <johnspencer15@gmail.com>
2017-05-11 02:14:16 -0400

    show worst intersections on map

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,6 +1,9 @@
 // Creat map wrapper, we center point and zoom level
 var map = L.map('map').setView([40.7328, -73.9259], 11);
 
+// route points
+ROUTE_POINTS = []
+
 // On first load, retrieve geoJSON file
 // Display map
 $.getJSON('/static/geo_data/borough_zip_geo.json', function(data) {
@@ -19,7 +22,7 @@ function createButton(label, container) {
     return btn;
 }
 
-accessToken= 'pk.eyJ1Ijoiam9obnNwZW5jZXIxNSIsImEiOiJjajE2b2hhN2owMzl2MzRvNjhpdDM5bzk3In0.wr4EEzRfvpGTw6C9ltRZsw'
+accessToken = 'pk.eyJ1Ijoiam9obnNwZW5jZXIxNSIsImEiOiJjajE2b2hhN2owMzl2MzRvNjhpdDM5bzk3In0.wr4EEzRfvpGTw6C9ltRZsw'
 
 // Begin: map and control for click to add waypoints
 var control = L.Routing.control({
@@ -38,6 +41,7 @@ control.on('routeselected', function(e) {
     for (var i = 0; i < route.coordinates.length; i++) {
         routeArray.push([route.coordinates[i].lat,route.coordinates[i].lng]);
     }
+    send_data(routeArray);
     console.log(routeArray);
   });
 
@@ -98,9 +102,39 @@ var control1 = L.Routing.control({
         for (var i = 0; i < route.coordinates.length; i++) {
             routeArray.push([route.coordinates[i].lat,route.coordinates[i].lng]);
         }
+        send_data(routeArray);
         console.log(routeArray);
       })
-
     .addTo(map);
   // End: map and control for geocode route search
 });
+
+function send_data(data) {
+    for (index in ROUTE_POINTS) {
+        map.removeLayer(ROUTE_POINTS[index]);
+    }
+    $('#map').css('display', 'inline-block');
+    $('#loading-box').show().css('display', 'inline-block');
+    $.ajax({
+        'type': 'POST',
+        'url': '/worst_intersections',
+        'contentType': 'application/json',
+        'dataType': 'json',
+        'data': JSON.stringify(data)
+   }).done(function(points) {
+       $('#map').css('display', 'block');
+       $('#loading-box').hide()
+        var index;
+        console.log(points);
+        colors = ['rgb(255, 247, 0)', 'rgb(255, 211, 0)', 'rgb(255, 186, 0)', 'rgb(255, 125, 0)', 'rgb(255, 0, 0)']
+        ROUTE_POINTS = []
+        for (index in points) {
+            point = L.circle(points[index][0], {radius: 30}).addTo(map);
+            ROUTE_POINTS.push(point)
+            point._path.setAttribute('data-rate', points[index][1])
+            point._path.style.fill = colors[index]
+            point._path.style.stroke = colors[index]
+            console.log(point);
+        }
+   });
+}
